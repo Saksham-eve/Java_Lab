3be7df8ec9d61286f584acf6db3327127b82189f
Saksham-eve <53502298+Saksham-eve@users.noreply.github.com>
2020-03-11 20:08:38 +0545

    lab work

---
+++ Java-Lab4/Package/package_name/ClassOne.java
@@ -0,0 +1,10 @@
+//package one
+package package_name; 
+  
+public class ClassOne { 
+    public void methodClassOne() { 
+        int a=5;
+        System.out.println("Hello there its ClassOne"); 
+        System.out.println(a);
+    } 
+} 
